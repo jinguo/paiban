663ebdccb890a62c30a7edf908bf3369085d346b
张锞卫 <janggwazkw@gmail.com>
2018-04-23 09:42:27 +0800

    add 3

--- src/App.js
+++ src/App.js
@@ -158,11 +158,94 @@ class App extends Component {
       }
     }
   }
+  handleCircleImage3 = () => {
+    let tpl2num = 0;
+    let tpl2li = '';
+    for (let i = 0; i < this.state.fileLength / 14; i++) {
+      tpl2num++;
+      tpl2li += '<div style="margin-top: 80px"><img id="img1" src="http://assets.janggwa.cn/1(' + tpl2num + ').jpg?t=' + new Date() + '" style="width: 750px" /></div>';
+      if (tpl2num === this.state.fileLength) {
+        return tpl2li;
+      }
+      tpl2num++;
+      tpl2li += '<div style="text-align: center; margin: 40px 0"><img src="http://assets.janggwa.cn/%E6%A8%A1%E6%9D%BF3%E7%B4%A0%E6%9D%901.png?t" style="width: 610px" /></div><div style="text-align: right"><img id="img2" src="http://assets.janggwa.cn/1(' + tpl2num + ').jpg?t=' + new Date() + '" style="width: 560px" /></div>';
+      if (tpl2num === this.state.fileLength) {
+        return tpl2li;
+      }
+      tpl2num++;
+      tpl2li += '<div style="display: flex;justify-content: flex-start; margin-top: 60px"><div><img id="img3" src="http://assets.janggwa.cn/1(' + tpl2num + ').jpg?t=' + new Date() + '" style="width: 560px" /></div><div style="margin-left: 70px"><img src="http://assets.janggwa.cn/%E6%A8%A1%E6%9D%BF3%E7%B4%A0%E6%9D%902.png" style="width: 24px" /></div></div>';
+      if (tpl2num === this.state.fileLength) {
+        return tpl2li;
+      }
+      tpl2num++;
+      tpl2li += '<div style="margin-top: 30px;"><div><img src="http://assets.janggwa.cn/%E6%A8%A1%E6%9D%BF3%E7%B4%A0%E6%9D%903.png" style="width: 480px" /></div></div><div style="margin-top: 50px"><img id="img4" src="http://assets.janggwa.cn/1(' + tpl2num + ').jpg?t=' + new Date() + '" style="width: 750px" /></div>';
+      if (tpl2num === this.state.fileLength) {
+        return tpl2li;
+      }
+      tpl2num++;
+      tpl2li += '<div style="position: relative;margin-top: 50px;text-align: center;"><img id="img5" src="http://assets.janggwa.cn/1(' + tpl2num + ').jpg?t=' + new Date() + '" style="width: 530px" /><div style="position: absolute; top: 10px;left: 60px;"><img src="http://assets.janggwa.cn/%E6%A8%A1%E6%9D%BF3%E7%B4%A0%E6%9D%904.png" style="width: 350px" /></div></div>';
+      if (tpl2num === this.state.fileLength) {
+        return tpl2li;
+      }
+      tpl2num++;
+      tpl2li += '<div style="margin-top: 30px; margin-left: 110px;"><img src="http://assets.janggwa.cn/%E6%A8%A1%E6%9D%BF3%E7%B4%A0%E6%9D%905.png" style="width: 400px" /></div><div style="margin-top: 60px"><img id="img6" src="http://assets.janggwa.cn/1(' + tpl2num + ').jpg?t=' + new Date() + '" style="width: 750px" /></div>';
+      if (tpl2num === this.state.fileLength) {
+        return tpl2li;
+      }
+      tpl2num++;
+      tpl2li += '<div style="position: relative; margin-top: 60px"><img id="img7" src="http://assets.janggwa.cn/1(' + tpl2num + ').jpg?t=' + new Date() + '" style="width: 620px" /><div style="position: absolute; bottom: -60px; right: 10px;"><img src="http://assets.janggwa.cn/%E6%A8%A1%E6%9D%BF3%E7%B4%A0%E6%9D%906.png" style="width: 430px" /></div></div>';
+      if (tpl2num === this.state.fileLength) {
+        return tpl2li;
+      }
+      tpl2num++;
+      tpl2li += '<div style="margin-top: 120px"><img id="img8" src="http://assets.janggwa.cn/1(' + tpl2num + ').jpg?t=' + new Date() + '" style="width: 750px" /></div>';
+      if (tpl2num === this.state.fileLength) {
+        return tpl2li;
+      }
+      tpl2num++;
+      tpl2li += '<div style="margin-top: 30px; text-align: center"><img src="http://assets.janggwa.cn/%E6%A8%A1%E6%9D%BF3%E7%B4%A0%E6%9D%907.png" style="width: 410px" /></div><div style="margin-top: 20px"><img id="img9" src="http://assets.janggwa.cn/1(' + tpl2num + ').jpg?t=' + new Date() + '" style="width: 750px" /></div>';
+      if (tpl2num === this.state.fileLength) {
+        return tpl2li;
+      }
+      tpl2num++;
+      tpl2li += '<div style="margin-top: 80px; text-align: right"><img id="img10" src="http://assets.janggwa.cn/1(' + tpl2num + ').jpg?t=' + new Date() + '" style="width: 630px" /></div><div style="margin-top: 20px; text-align: right"><img src="http://assets.janggwa.cn/%E6%A8%A1%E6%9D%BF3%E7%B4%A0%E6%9D%908.png" style="width: 400px" /></div>';
+      if (tpl2num === this.state.fileLength) {
+        return tpl2li;
+      }
+      tpl2num++;
+      tpl2li += '<div style="margin-top: 30px"><img id="img11" src="http://assets.janggwa.cn/1(' + tpl2num + ').jpg?t=' + new Date() + '" style="width: 750px" /></div>';
+      if (tpl2num === this.state.fileLength) {
+        return tpl2li;
+      }
+      tpl2num++;
+      tpl2li += '<div style="display: flex; margin-top: 40px;"><div style="margin-left: 40px"><img id="img12" src="http://assets.janggwa.cn/1(' + tpl2num + ').jpg?t=' + new Date() + '" style="width: 480px" /></div><div style="margin-left: 70px;"><img src="http://assets.janggwa.cn/%E6%A8%A1%E6%9D%BF3%E7%B4%A0%E6%9D%909.png" style="width: 80px" /></div></div>';
+      if (tpl2num === this.state.fileLength) {
+        return tpl2li;
+      }
+    }
+  }
   handleCreateResult = () => {
     let tpl2li = this.handleCircleImage();
+    let tpl3li = this.handleCircleImage3()
     const tpl1 =  '<div id="image" style="width: 750px; margin: 0 auto"><div style="text-align: center; margin-top: 40px"><img src="http://assets.janggwa.cn/%E6%A8%A1%E6%9D%BF1%E7%B4%A0%E6%9D%901.png" style="width: 350px" /></div><div style="margin: 40px 0"><img id="img1" src="http://assets.janggwa.cn/1(1).jpg" style="width: 750px" /></div><div style="text-align: right"><img src="http://assets.janggwa.cn/%E6%A8%A1%E6%9D%BF1%E7%B4%A0%E6%9D%902.png" style="width: 148px; margin-right: 48px" /></div><div style="display: flex;justify-content: space-between;align-items: center"><div><img src="http://assets.janggwa.cn/%E6%A8%A1%E6%9D%BF1%E7%B4%A0%E6%9D%903.png" style="width: 130px; margin-left:24px" /></div><div><div style="margin: 40px 0; text-align: right"><img id="img2" src="http://assets.janggwa.cn/1(2).jpg" style="width: 560px" /></div><div style="margin: 40px 0; text-align: right"><img id="img3" src="http://assets.janggwa.cn/1(3).jpg" style="width: 560px" /></div></div></div><div style="margin: 0 0 40px"><img id="img4" src="http://assets.janggwa.cn/1(4).jpg" style="width: 750px" /></div><div style="text-align: center; margin: 40px 0"><img src="http://assets.janggwa.cn/%E6%A8%A1%E6%9D%BF1%E7%B4%A0%E6%9D%904.png" style="width: 384px" /></div><div style="display: flex;align-items: center"><img id="img5" src="http://assets.janggwa.cn/1(5).jpg" style="width: 556px" /><img src="http://assets.janggwa.cn/%E6%A8%A1%E6%9D%BF1%E7%B4%A0%E6%9D%905.png" style="width: 184px;margin-left: 10px" /></div><div style="margin-top: 40px"><img id="img6" src="http://assets.janggwa.cn/1(6).jpg" style="width: 750px" /></div></div>';
     const tpl2 = '<div id="image" style="width: 750px; margin: 0 auto; padding-bottom: 150px">' + tpl2li + '</div>';
-    document.querySelector('.result').innerHTML = this.state.value === 1 ? tpl1: tpl2;
+    const tpl3 = '<div id="image" style="width: 750px; margin: 0 auto; padding-bottom: 150px">' + tpl3li + '</div>';
+    let content = '';
+    switch (this.state.value) {
+      case 1:
+        content = tpl1;
+        break;
+      case 2:
+        content = tpl2;
+        break;
+      case 3:
+        content = tpl3;
+        break;
+      default:
+        break;
+
+    }
+    document.querySelector('.result').innerHTML = content;
   }
   handleCreateCode = () => {
     const code  = document.querySelector('.result').innerHTML;
